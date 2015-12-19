5aee2690de98beabc3d9204f19a936019eea9cd5
Martin <ickobombata@gmail.com>
2015-12-19 21:37:53 +0200

    Adding examples.

--- arduino/wifi/wifi.ino
+++ arduino/wifi/wifi.ino
@@ -1,32 +1,11 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial esp8266(2,3);
-
-void setup() 
-{
-  Serial.begin(9600);
-  esp8266.begin(9600);
- 
+int ledPin = 0;
+void setup() {
+  pinMode(ledPin, OUTPUT);
 }
-
-void loop()
-{
-  while(esp8266.available())
-  {
-    char c = esp8266.read();
-    Serial.write(c);
-  }
-
-  if(Serial.available())
-  {
-    delay(1000);
-
-    String command="";
-
-    while(Serial.available())
-    {
-      command += (char)Serial.read();
-    }
-    esp8266.println(command);
-  }
+ 
+void loop() {
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                  // wait for a second
 }
