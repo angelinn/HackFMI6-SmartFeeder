eeddecdd7121467c96fccc87de032463fdaef8c1
betrakiss <betrakiss@hotmail.com>
2015-12-19 16:54:43 +0200

    Adding visual studio project.

---
+++ SmartFeeder/SmartFeeder/SmartFeeder.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SmartFeeder.ino
+ Created:	12/19/2015 4:53:49 PM
+ Author:	betra
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
